6c0ba18f4fb2f62543bac72ae2db5e36a73f1e7d
Herbert Bintoroe <herbertbintoroe@gmail.com>
2020-07-02 10:56:23 +0700

    Change message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello There Cecet!
+          Hello There!
         </p>
         <a
           className="App-link"
